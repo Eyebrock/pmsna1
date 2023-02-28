41480269dd2f2236d93b286cf1e6bafc4188f4cc
Eyebrock <eyebrock2913@gmail.com>
2023-02-27 22:56:31 -0600

    onboarding terminado

--- lib/screens/OnboardingPage.dart
+++ lib/screens/OnboardingPage.dart
@@ -21,7 +21,7 @@ class OnboardingPage extends StatelessWidget {
     CardPlanetData(
       title: "Perfil de Ingreso",
       subtitle: "El y la estudiante al ingresar, deberá tener habilidades matemáticas y lógicas, capacidad de análisis y síntesis de información, habilidades de investigación, así como interés por la computación y la programación, disposición para trabajar en equipo y sentido de compromiso social.",
-      image: const AssetImage("assets/images/perfil.png"),
+      image: const AssetImage("assets/perfil.jpg"),
       backgroundColor: Colors.white,
       titleColor: Colors.purple,
       subtitleColor: const Color.fromRGBO(0, 10, 56, 1),
@@ -30,7 +30,7 @@ class OnboardingPage extends StatelessWidget {
     CardPlanetData(
       title: "Residencias Profesionales",
       subtitle: "A partir del 80% de créditos podrá realizar residencias profesionales, en donde pondrá en práctica sus conocimientos adquiridos en organizaciones gubernamentales, privadas, así como en la industria de software.",
-      image: const AssetImage("assets/images/mats.png"),
+      image: const AssetImage("assets/mats.jpg"),
       backgroundColor: const Color.fromRGBO(71, 59, 117, 1),
       titleColor: Colors.yellow,
       subtitleColor: Colors.white,
@@ -39,16 +39,16 @@ class OnboardingPage extends StatelessWidget {
     CardPlanetData(
       title: "Al egresar",
       subtitle: "El y la Ingeniero/a en Sistemas Computacionales cuenta un amplio campo de trabajo, dado quela industrial del software requiere profesionistas creativos con capacidades para desarrollar sistemas, diseñar y administrar redes de computadoras, diseño de sitios web, tecnologías móviles, internet, videojuegos y seguridad de información tanto en el sector público, privado o como consultor/a especializado.",
-      image: const AssetImage("assets/images/egreso.png"),
+      image: const AssetImage("assets/egreso.jpg"),
       backgroundColor: Color.fromARGB(255, 7, 138, 186),
       titleColor: Color.fromARGB(255, 59, 255, 170),
-      subtitleColor: Color.fromARGB(255, 231, 10, 201),
+      subtitleColor: Color.fromARGB(255, 250, 243, 249),
       background: LottieBuilder.asset("assets/animation/bg-3.json"),
     ),
     CardPlanetData(
       title: "Modulos de Especialidad",
       subtitle: "Tecnologias emergentes para la toma de decisiones & Gestion de datos y tecnologias e informacion",
-      image: const AssetImage("assets/images/carrera.png"),
+      image: const AssetImage("assets/carrera.jpg"),
       backgroundColor: Color.fromARGB(255, 112, 29, 15),
       titleColor: Color.fromARGB(255, 206, 52, 5),
       subtitleColor: Color.fromARGB(255, 242, 238, 163),
